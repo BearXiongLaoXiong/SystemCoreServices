26eb9c1470c677998cc481e41babcb80999816fd
熊旭 <sh17951@outlook.com>
2017-09-19 17:06:18 +0800

    index-carousel

---
+++ 01 User Interface/HkEbPortal/Scripts/Home.js
@@ -0,0 +1,9 @@
+﻿layui.use('carousel', function () {
+    var carousel = layui.carousel;
+    carousel.render({
+        elem: '#indexcarouselimage'
+        , width: '100%'
+        , height: '800px'
+        , interval: 5000
+    });
+});
