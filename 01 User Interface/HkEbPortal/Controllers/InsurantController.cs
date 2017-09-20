fd97043a38920310e3503e6c36a5abae4b6a4f06
li qie <markdavid_le@outlook.com>
2017-09-20 17:09:47 +0800

    123

--- 01 User Interface/HkEbPortal/Controllers/InsurantController.cs
+++ 01 User Interface/HkEbPortal/Controllers/InsurantController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using HKEBPortal.BusinessLogic;
+using HKEBPortal.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,12 +10,14 @@ namespace HkEbPortal.Controllers
 {
     public class InsurantController : Controller
     {
+        private InsurantBusiness insurant = new InsurantBusiness();
         // GET: Insurant
         public ActionResult Index()
         {
             // 默认返回员工详细信息
+            List<SPEH_FMDT_FAMILY_DETL_LIST_WEB_RESULT> list = insurant.GetMemberInfoList();
 
-            return View();
+            return View(list);
         }
     }
 }
