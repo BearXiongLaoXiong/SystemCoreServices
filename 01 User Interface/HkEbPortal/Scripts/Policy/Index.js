a0c38735dd65861d26f6685165292fd2654c7f61
熊旭 <sh17951@outlook.com>
2017-09-25 09:52:31 +0800

    policy

--- 01 User Interface/HkEbPortal/Scripts/Policy/Index.js
+++ 01 User Interface/HkEbPortal/Scripts/Policy/Index.js
@@ -1,26 +1,25 @@
 ﻿layui.use('element', function () {
     var element = layui.element;
 
-    //一些事件监听
-    element.on('tab(tabMember)', function (data) {
-        console.log(data.va());
-    });
 });
 
 
 layui.use('form', function () {
     var form = layui.form;
-    search('', "代爱武", 0);
+    var memeKy = getUrlParam("memeKy");
+    console.log("memeKy = " + memeKy);
+    laodview('', (memeKy !== null && memeKy !== "") ? memeKy : "");
+
     form.on('radio', function (data) {
         console.log(data.elem); //得到radio原始DOM对象
         console.log(data.value); //被点击的radio的value值
-        search('', "代爱武", 0);
+        laodview(data.value, "");
     });
 
 });
 
 
-function search(status, name, index) {
+function laodview(status, memeKy) {
     layer.msg('laoding.....', {
         width: '100px',
         icon: 16,
@@ -29,14 +28,16 @@ function search(status, name, index) {
         anim: 5
     });
     $.get("FindView",
-        { status: status, name: name },
+        { status: status, memeKy: memeKy },
         function (data) {
-
+            var index = 0;
             //追加人员名称
             $("#divMemberNames").empty();
             $.each(data.names, function (key, value) {
-                console.log(key);
-                $("#divMemberNames").append('<button name="btnMemberName" class="layui-btn ' + (index !== key ? "layui-btn-primary" : "") + '" onclick="search(\'\',\'' + value.Name + '\',' + key + ')">' + value.Name + '</button>');
+                //首次加载或者选择后btn class为选中状态
+                var style = memeKy === "" && index === 0 ? "" : memeKy === value.MemeKy ? "" : "layui-btn-primary";
+                $("#divMemberNames").append('<button name="btnMemberName" class="layui-btn ' + style + '" onclick="laodview(\'\',\'' + value.MemeKy + '\')">' + value.Name + '</button>');
+                index++;
             });
 
             $("#memberTableBody").empty();
@@ -54,10 +55,17 @@ function search(status, name, index) {
                     default: statu = '<span class="layui-badge layui-bg-blue">全部</span>';; break;
                 }
                 html += '<td>' + statu + '</td>';
-                html += '<td style="padding: 0px;" align="center" data-off="true"><div class="layui-table-cell laytable-cell-1-4"> <a class="layui-btn layui-btn-primary layui-btn-mini" lay-event="detail" i18n="public.common.detail">查看</a> <a class="layui-btn layui-btn-danger layui-btn-mini" lay-event="del" i18n="public.common.edit" href="../Policy/Detail?plplKy=' + value.PLPL_KY + '&memeKy=' + value.MEME_KY + '" target="_blank">编辑</a> </div></td>';
+                html += '<td style="padding: 0px;" align="center" data-off="true"><div class="layui-table-cell laytable-cell-1-4"> <a class="layui-btn layui-btn-primary layui-btn-mini" lay-event="detail" i18n="public.common.detail">查看</a> <a class="layui-btn layui-btn-danger layui-btn-mini" lay-event="del" i18n="public.common.edit" href="../Policy/Detail?plplKy=' + value.PLPL_KY + '&memeKy=' + value.MEME_KY + '">编辑</a> </div></td>';
                 html += '</tr>';
                 $("#memberTableBody").append(html);
             });
             setTimeout(function () { layer.closeAll() }, 300);
         });
+}
+
+
+function getUrlParam(name) {
+    var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+    var r = window.location.search.substr(1).match(reg);  //匹配目标参数
+    if (r !== null) return unescape(r[2]); return null; //返回参数值
 }
