9227c16dcc0c0bccd7efea2d1f2ba9cdd4dc4447
熊旭 <sh17951@outlook.com>
2017-09-26 16:33:32 +0800

    policy

--- 01 User Interface/HkEbPortal/Models/EB_PORTAL/SPEH_PLME_PLOCY_MEME_INFO_LIST_WEB.cs
+++ 01 User Interface/HkEbPortal/Models/EB_PORTAL/SPEH_PLME_PLOCY_MEME_INFO_LIST_WEB.cs
@@ -9,8 +9,8 @@ namespace HkEbPortal.Models.EB_PORTAL
     {
         public string pEHUSER { get; set; }
         public string pSYSV_PLPL_STS { get; set; }
-        public string pPLPL_KY { get; set; }
-        public string pPDCT_ID { get; set; }
+        //public string pPLPL_KY { get; set; }
+        //public string pPDCT_ID { get; set; }
         public string lang { get; set; }
         public string pMEME_KY { get; set; }
     }
@@ -51,19 +51,5 @@ namespace HkEbPortal.Models.EB_PORTAL
         public string MEME_KY { get; set; }
     }
 
-    public class SPEH_PLME_PLOCY_MEME_INFO_LIST_WEB_RESULT4
-    {
-        public string MEME_KY { get; set; }
-        public string PLPL_KY { get; set; }
-        public string PDPD_ID { get; set; }
-        public string PDCT_ID { get; set; }
-        public string PDPD_NAME { get; set; }
-        public string PDPD_LEVEL { get; set; }
-        public string PLPD_DEF_IND { get; set; }
-        public string DFF_IND { get; set; }
-        public string PLME_AMT { get; set; }
-        public string EMP_AMT { get; set; }
-        public string EMPYEE_AMT { get; set; }
-        public string TXTX_AMT { get; set; }
-    }
+    
 }
