1895421e9b70fbbecfaec5346b146ee926215119
熊旭 <sh17951@outlook.com>
2017-10-02 12:02:10 +0800

    login

--- 01 User Interface/HkEbPortal/Scripts/Home/Home.js
+++ 01 User Interface/HkEbPortal/Scripts/Home/Home.js
@@ -1,17 +1,15 @@
-﻿//轮播
-layui.use('carousel', function () {
+﻿
+layui.use(['form', 'carousel'], function () {
+    var form = layui.form;
     var carousel = layui.carousel;
+
+    //轮播
     carousel.render({
         elem: '#indexcarouselimage'
         , width: '100%'
         , height: '750px'
         , interval: 5000
     });
-});
-
-
-layui.use('form', function () {
-    var form = layui.form;
 
     form.on('submit(btnLogin)', function (data) {
         //layer.msg(JSON.stringify(data.field));
@@ -26,5 +24,39 @@ layui.use('form', function () {
         return false;
     });
 
+    form.on('submit(btnLoginUp)', function (data) {
+        layer.msg(JSON.stringify(data.field));
+        $.post("Login",
+            { txtMember: data.field.txtMember, txtPassword: data.field.txtPassword },
+            function (result) {
+                if (result.Code === 0)
+                    window.location.href = "../Home/Index";
+                else
+                    layer.msg(result.Msg);
+            });
+        return false;
+    });
+
 });
 
+function signIn() {
+    $("#signIndiv").addClass("selected");
+    $("#singUpdiv").removeClass("selected");
+
+    $("#signInMessage").show();
+    $("#signUpMessage").hide();
+
+    $("#signInForm").show();
+    $("#signUpForm").hide();
+}
+
+function signUp() {
+    $("#signIndiv").removeClass("selected");
+    $("#singUpdiv").addClass("selected");
+
+    $("#signInMessage").hide();
+    $("#signUpMessage").show();
+
+    $("#signInForm").hide();
+    $("#signUpForm").show();
+}
