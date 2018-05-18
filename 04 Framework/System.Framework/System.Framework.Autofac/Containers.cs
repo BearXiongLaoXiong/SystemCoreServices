bfd24d86ed0ccea1fa6699aaf7ce2d19511940a2
熊旭 <sh17951@outlook.com>
2018-05-18 15:49:40 +0800

    升级.net Framework 4.7

--- 04 Framework/System.Framework/System.Framework.Autofac/Containers.cs
+++ 04 Framework/System.Framework/System.Framework.Autofac/Containers.cs
@@ -1,19 +1,26 @@
-﻿using System.Configuration;
-using Autofac;
+﻿using Autofac;
 using Autofac.Configuration;
 using Autofac.Core;
+using Microsoft.Extensions.Configuration;
 
 namespace System.Framework.Autofac
 {
     public class Containers
     {
         private static IContainer _container;
+
         public static T Resolve<T>()
         {
-            if (_container == null)
-                InitializeComponent();
+            if (_container == null) InitializeComponent();
             return _container.Resolve<T>();
         }
+
+        public static T Resolve<T>(string serviceName)
+        {
+            if (_container == null) InitializeComponent();
+            return _container.ResolveNamed<T>(serviceName);
+        }
+
         public static T Resolve<T>(params Parameter[] parameters)
         {
             if (_container == null)
@@ -21,14 +28,20 @@ namespace System.Framework.Autofac
             return _container.Resolve<T>(parameters);
         }
 
+        public static T Resolve<T>(string serviceName, params Parameter[] parameters)
+        {
+            if (_container == null) InitializeComponent();
+            return _container.ResolveKeyed<T>(serviceName, parameters);
+        }
+
         private static void InitializeComponent()
         {
+            var config = new ConfigurationBuilder();
+            config.AddJsonFile("Configs/autofac.json");
+
+            var module = new ConfigurationModule(config.Build());
             var builder = new ContainerBuilder();
-            //builder.Register(c => new TestBll()).As<ITestBll>();
-            //var configurationFile = ConfigurationManager.AppSettings["AutofacConfigurationFile"];
-            //if (string.IsNullOrEmpty(configurationFile)) throw new ArgumentNullException($"not found AutofacConfigurationFile!");
-            builder.RegisterModule(new ConfigurationSettingsReader("autofac")); 
-            //builder.RegisterModule(new ConfigurationSettingsReader("BusinessLogic", configurationFile));
+            builder.RegisterModule(module);
             _container = builder.Build();
         }
     }
